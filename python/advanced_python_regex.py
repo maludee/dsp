a05a3c34de8decbf2fb8b84f18b039ed0d37f6cc
Malu Dee <maludee@malus-air.home>
2016-12-17 14:30:17 +0000

    updated code

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,7 +1,8 @@
-### Q1:
+# Q1:
 
 import csv
 from collections import Counter
+import itertools
 
 # Open faculty csv file
 input_stream = open('faculty.csv')
@@ -15,10 +16,19 @@ degrees = [row[1] for row in reader]
 neat = [d.strip(' ') for d in degrees]
 neat = [n.replace('.', '') for n in neat]
 
-# Count unique instances of degree types
-for (k,v) in Counter(neat).iteritems():
-    print "%s: %d" % (k, v)
+# Separate degrees with spaces
+sept = []
+for n in neat:
+    f = n.split(' ')
+    sept.append(f)
 
+# Merge all lists into one list of degrees
+merged = list(itertools.chain.from_iterable(sept))
+
+# Count unique instances of degree types
+for (k,v) in Counter(merged).iteritems():
+    print "%s: %d" % (k, v)
+    
     
 ### Q2:   
 
@@ -34,13 +44,13 @@ reader.next() # skips header row
 titles = [row[2] for row in reader]
 
 # Fix typo 'Assistant Professor is Biostatistics
-newtitles = [t.replace('is', 'of') for t in titles]
+newtitles = [t.replace(' is ', ' of ') for t in titles]
 
 # Count unique titles
 for (k,v) in Counter(newtitles).iteritems():
     print "%s: %d" % (k, v)
- 
-
+    
+    
 ### Q3:
 
 import csv

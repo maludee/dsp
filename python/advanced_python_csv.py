436ca46cfa7379d6c8d638a21a59f18193ca9d6e
Malu Dee <maludee@malus-air.home>
2016-12-17 15:21:09 +0000

    added code

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,16 @@
-PLACE YOUR CODE HERE
+import csv
+
+# Open faculty csv file
+input_stream = open('faculty.csv')
+reader = csv.reader(input_stream, delimiter = ',')
+
+# Create list of email addresses
+reader.next() # skips header row
+emails = [row[3] for row in reader]
+
+# Write email adresses to emails.csv file
+with open ('emails.csv', 'w') as f:
+    writer = csv.writer(f)
+    for val in emails:
+        writer.writerow([val])
+        
